7dcacc6770a9bc8c705fb3762ed94a2f04762174
gkalinikos <giorgoskallis21@gmail.com>
2019-12-06 16:29:18 +0200

    finished the lecture

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+  <div class="container">
+    <a class="navbar-brand" href="<?php echo URLROOT?>"><?php echo SITENAME?></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item ">
+                <a class="nav-link" href="<?php echo URLROOT?>/pages/index">Home </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo URLROOT?>/pages/about">About</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <?php if (isset($_SESSION['user_id'])) : ?>
+                <li class="nav-item">
+                    <span class="nav-link">Welcome <?php echo $_SESSION['user_name']; ?> </span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT?>/users/logout">Logout </a>
+                </li>
+            <?php else : ?>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo URLROOT?>/users/register">Register </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo URLROOT?>/users/login">Login</a>
+            </li>
+        <?php endif; ?>
+        </ul>
+    </div>
+  </div>
+</nav>
